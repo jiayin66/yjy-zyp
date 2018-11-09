cdc41af48e7513dd56a57df1a44647ac400dc4dd
jiayin <501023696@qq.com>
2018-11-09 17:30:33 +0800

    1

---
+++ src/main/java/com/blw/yjyzyp/YjyZypApplication.java
@@ -0,0 +1,12 @@
+package com.blw.yjyzyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjyZypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjyZypApplication.class, args);
+	}
+}
